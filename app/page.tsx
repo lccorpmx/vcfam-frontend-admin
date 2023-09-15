116e17df38ee7231e1f4136b313aea46684ca73d
lccorpmx <lcccorpmx@gmail.com>
2023-09-15 12:28:17 -0600

    add base project

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+       <Head>
+    <title>VCFAM</title>
+    <meta name="description" content="VCFAM" />
+    <link rel="manifest" href="/manifest.json" />
+  </Head>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+      <h1 className=''>VCFAM</h1>
+    </main>
+      <h1 className=''>VCFAM</h1>
+    </main>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+const runtimeCaching = require("next-pwa/cache");
+const withPWA = require('next-pwa')({
+ dest: 'public', 
+ register: true,
+ skipWaiting: true,
+ runtimeCaching
+})
+
+module.exports = withPWA({
+ // other congigs
+ reactStrictMode: false
+})
+
+module.exports = nextConfig
