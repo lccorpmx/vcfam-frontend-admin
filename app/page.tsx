e9381c85503d609b357d91108d7e458ab586228a
lccorpmx <lcccorpmx@gmail.com>
2023-09-15 12:44:29 -0600

    add base to project

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <Head>
-    <title>VCFAM</title>
+      <header>
+      <title>VCFAM</title>
     <meta name="description" content="VCFAM" />
     <link rel="manifest" href="/manifest.json" />
-  </Head>
+      </header>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
       <h1 className=''>VCFAM</h1>
