528f4cd987574fd6dd379c9fe50ec9d4dbff14a9
lccorpmx <lcccorpmx@gmail.com>
2023-12-19 22:17:22 -0600

    add first version beta vcfamadmin

--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -1,9 +1,13 @@
 // components/TextEditor.js
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css'; // Estilo por defecto de Quill
+import dynamic from "next/dynamic";
+import { useState, useMemo } from "react";
+
 
 const TextEditor = ({ value, onChange }: {value:any, onChange:any}) => {
     console.log(onChange)
+    const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), { ssr: false }),[]);
   return (
     <ReactQuill
       theme="snow" // Puedes cambiar el tema según tus preferencias
